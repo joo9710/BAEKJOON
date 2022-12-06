a27da687bd6feccb13fcca432298e54356f4788b
joo9710 <joo9710@naver.com>
2022-12-06 19:58:54 +0900

    그리디 알고리즘 ~ing

---
+++ Greedy/b11399.java
@@ -0,0 +1,35 @@
+package Greedy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class b11399 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int arr[] = new int[N];
+        int sum=0;
+
+        for(int i=0; i<N; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for(int i=1; i<N; i++) {
+                arr[i] = arr[i] + arr[i - 1];
+        }
+
+        for(int i=0; i<N; i++) {
+            sum +=arr[i];
+        }
+
+        System.out.println(sum);
+
+
+
+    }
+}
